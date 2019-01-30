2212d452c28e19ef3dd756f74cd1754a70b41634
Evan Simmons <esims89@gmail.com>
2019-01-30 08:53:09 -0800

    Add gpx test

--- tests/gpx.rs
+++ tests/gpx.rs
@@ -1,5 +1,5 @@
 #![feature(custom_attribute)]
-use serde_xml_rs::from_reader;
+use serde_xml_rs::{from_reader, to_string};
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
@@ -18,4 +18,11 @@ fn read_fixture(filename: &str) -> Result<GpxType, Box<dyn Error>> {
 fn test_read_gpx_sample() {
     let gpx = read_fixture("gpx_sample.gpx").unwrap();
     eprintln!("{:#?}", gpx);
+}
+
+#[test]
+fn test_rw_gpx_sample() {
+    let gpx = read_fixture("gpx_sample.gpx").unwrap();
+    let str_gpx_out = to_string(&gpx).unwrap();
+    eprintln!("{:#?}", str_gpx_out);
 }
